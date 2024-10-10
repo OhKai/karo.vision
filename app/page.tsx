770dab9a359ec428dad0b4b94dd6a1f58e6d267e
Kai Rohwer <rohwer.kai@icloud.com>
2024-10-10 18:44:11 +0200

    Add header-nav and search-bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-import { Settings, TvMinimalPlay, Image as LImage, Music } from "lucide-react";
+import { Settings } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import Link from "next/link";
+import HeaderNav from "@/components/header-nav";
+import SearchBar from "@/components/search-bar";
 
 const Home = () => {
   return (
     <div className="grid grid-rows-[70px_1fr] justify-items-center min-h-screen pb-20 font-[family-name:var(--font-geist-sans)]">
       <header className="grid grid-cols-3 items-center justify-items-center w-full px-6 text-muted-foreground border-b border-secondary">
-        <div className="flex items-center gap-2 justify-self-start">
+        <div className="flex items-center gap-2 justify-self-start select-none">
           <Image
             src="/logo.png"
             alt="Karo.Vision Home Cloud"
@@ -20,34 +21,19 @@ const Home = () => {
             <span>Home&nbsp;Cloud</span>
           </h2>
         </div>
-        <nav className="flex gap-3">
-          <Button
-            asChild
-            size="icon"
-            variant="ghost"
-            data-active="true"
-            className="relative data-[active]:text-secondary-foreground after:block after:w-1 after:h-1 after:absolute after:-bottom-1.5 after:rounded-full after:bg-secondary-foreground"
-          >
-            <Link href="/">
-              <TvMinimalPlay />
-            </Link>
-          </Button>
-          <Button asChild size="icon" variant="ghost">
-            <Link href="/photos">
-              <LImage />
-            </Link>
-          </Button>
-          <Button asChild size="icon" variant="ghost">
-            <Link href="/music">
-              <Music />
-            </Link>
-          </Button>
-        </nav>
-        <Button className="justify-self-end" size="icon" variant="ghost">
+        <HeaderNav />
+        <Button
+          className="justify-self-end"
+          size="icon"
+          variant="ghost"
+          title="Settings"
+        >
           <Settings />
         </Button>
       </header>
-      <main className="flex flex-col gap-8 items-center sm:items-start"></main>
+      <main className="flex flex-col gap-8 items-center py-4">
+        <SearchBar />
+      </main>
     </div>
   );
 };
---
+++ components/search-bar.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { SlidersHorizontal } from "lucide-react";
+import { Button } from "./ui/button";
+import { useState } from "react";
+
+export type SearchSortBy =
+  | "name-asc"
+  | "name-desc"
+  | "date-asc"
+  | "date-desc"
+  | "size-asc"
+  | "size-desc"
+  | "random";
+
+const SearchBar = () => {
+  const [sortBy, setSortBy] = useState<SearchSortBy>("name-asc");
+
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <div className="grid grid-cols-[40px_1fr_40px] gap-2 peer">
+        <Input
+          type="search"
+          placeholder="Search Files, Tags, Notes ..."
+          className="w-[590px] rounded-full h-[42px] px-5 col-start-2 font-medium focus-within:shadow-[0_2px_8px_-2px_hsl(var(--foreground)_/_8%)]"
+        />
+        <Button
+          size="icon"
+          variant="ghost"
+          className="text-muted-foreground"
+          title="Search Options"
+        >
+          <SlidersHorizontal />
+        </Button>
+      </div>
+      <ToggleGroup
+        type="single"
+        className="justify-stretch w-[278px] peer-focus-within:opacity-100 peer-focus-within:scale-100 peer-focus-within:translate-y-0 opacity-0 scale-95 translate-y-[-10px] transition-all"
+        value={sortBy.split("-")[0] as "name" | "date" | "size" | "random"}
+        onMouseDown={(e) => e.preventDefault()}
+        onValueChange={(value) => {
+          if (!value) {
+            // Toggle same sort type order
+            const [sort, order] = sortBy.split("-");
+            setSortBy(
+              (sort +
+                (order === "asc"
+                  ? "-desc"
+                  : order
+                    ? "-asc"
+                    : "")) as SearchSortBy,
+            );
+          } else {
+            // Change sort type
+            setSortBy(
+              (value === "random" ? "random" : value + "-asc") as SearchSortBy,
+            );
+          }
+        }}
+      >
+        <ToggleGroupItem
+          value="name"
+          aria-label="Toggle Name"
+          className="flex-1"
+        >
+          Name{" "}
+          {sortBy.startsWith("name")
+            ? sortBy.endsWith("asc")
+              ? "↑"
+              : "↓"
+            : null}
+        </ToggleGroupItem>
+        <ToggleGroupItem
+          value="date"
+          aria-label="Toggle Date"
+          className="flex-1"
+        >
+          Date{" "}
+          {sortBy.startsWith("date")
+            ? sortBy.endsWith("asc")
+              ? "↑"
+              : "↓"
+            : null}
+        </ToggleGroupItem>
+        <ToggleGroupItem
+          value="size"
+          aria-label="Toggle Size"
+          className="flex-1"
+        >
+          Size{" "}
+          {sortBy.startsWith("size")
+            ? sortBy.endsWith("asc")
+              ? "↑"
+              : "↓"
+            : null}
+        </ToggleGroupItem>
+        <ToggleGroupItem
+          value="random"
+          aria-label="Toggle Random"
+          className="flex-1"
+        >
+          Random
+        </ToggleGroupItem>
+      </ToggleGroup>
+    </div>
+  );
+};
+
+export default SearchBar;
